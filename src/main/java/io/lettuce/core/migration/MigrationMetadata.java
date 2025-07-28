df8179fffef4cf0fc2ea04fc4713d3ec88a8a739
entallaris <ef.ntallaris@gmail.com>
2025-07-28 12:41:59 +0300

    Add lettuce as submodule

--- src/main/java/io/lettuce/core/migration/MigrationMetadata.java
+++ src/main/java/io/lettuce/core/migration/MigrationMetadata.java
@@ -40,6 +40,7 @@ import io.netty.util.internal.logging.InternalLoggerFactory;
 public class MigrationMetadata {
     
     private static final int METADATA_SIZE = 50; // 2+2+46+2 bytes
+
     private static final int MAX_HOST_LEN = 46;
     private static final InternalLogger logger = InternalLoggerFactory.getInstance(MigrationMetadata.class);
     
